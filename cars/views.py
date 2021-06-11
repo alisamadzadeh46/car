a1c7831e01cdffacb39026642a3eb7fcc7a93b7f
alisamadzadeh <alisamadzadeh46@gmail.com>
2021-06-11 22:16:37 +0430

    fix car detail in index.html

--- cars/views.py
+++ cars/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 
+from .models import Cars
+
 
 def cars(request):
     return render(request, 'cars/cars.html')
 
 
 def car_detail(request, id):
+    # car = Cars.objects.filter(cars)
     return render(request, 'cars/detail.html')
