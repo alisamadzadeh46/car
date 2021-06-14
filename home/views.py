7e7040bcde2538fed1b550c524126f9a4e5465c3
alisamadzadeh <alisamadzadeh46@gmail.com>
2021-06-14 18:45:55 +0430

    update index.html and views.py

--- home/views.py
+++ home/views.py
@@ -8,13 +8,19 @@ def home(request):
     teams = Team.objects.all()
     featured_cars = Cars.objects.order_by('-created_data').filter(is_featured=True)
     latest_cars = Cars.objects.order_by('-created_data')
-    search_fields = Cars.objects.values('model', 'city', 'year', 'body_style')
+    model_search = Cars.objects.values_list('model', flat=True).distinct()
+    city_search = Cars.objects.values_list('city', flat=True).distinct()
+    year_search = Cars.objects.values_list('year', flat=True).distinct()
+    body_style_search = Cars.objects.values_list('body_style', flat=True).distinct()
 
     data = {
         'teams': teams,
         'featured_cars': featured_cars,
         'latest_cars': latest_cars,
-        'search_fields': search_fields,
+        'model_search': model_search,
+        'city_search': city_search,
+        'year_search': year_search,
+        'body_style_search': body_style_search,
 
     }
     return render(request, 'home/index.html', data)
