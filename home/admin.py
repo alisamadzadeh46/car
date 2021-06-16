63a322c880a141770df7bf5725460480a82cf6d6
alisamadzadeh <alisamadzadeh46@gmail.com>
2021-06-16 20:48:25 +0430

    create ContactAdmin in admin.py

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Team
+from .models import Team, Contact
 
 
 @admin.register(Team)
@@ -8,3 +8,10 @@ class TeamAdmin(admin.ModelAdmin):
     readonly_fields = ['image', ]
     search_fields = ['first_name', 'last_name', 'designation']
     list_filter = ['designation', ]
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'phone']
+    search_fields = ['name', 'email', 'phone']
+    list_filter = ['name', 'email', 'phone']
